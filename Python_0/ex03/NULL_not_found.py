16be0d4766226258fcdea27fb0ac9cac68dd332a
Ahlem NOURI <anouri@f4r10s13.clusters.42paris.fr>
2025-06-30 15:28:43 +0200

    ex03

--- Python_0/ex03/NULL_not_found.py
+++ Python_0/ex03/NULL_not_found.py
@@ -1,5 +1,21 @@
+
 def NULL_not_found(object: any) -> int:
     obj_type = type(object)
-    print(obj_type)
-    return(0)
-    # return(1)
+    if object == None:
+        print("Nothing:",object, obj_type)
+        return(0)
+    elif obj_type == float and object != object:
+        print("Cheese:",object, obj_type)
+        return(0)
+    elif object == 0 and obj_type == int:
+        print("Zero:",object, obj_type)
+        return(0)
+    elif object == "":
+        print("Empty:", obj_type)
+        return(0)
+    elif object == False:
+        print("Fake:",object, obj_type)
+        return(0)
+    print("Type not Found")
+    return(1)
+ 
