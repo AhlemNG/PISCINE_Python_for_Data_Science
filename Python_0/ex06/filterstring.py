562b96c28210a4f463365718234acab0a46e6db0
Ahlem NOURI <anouri@f4r1s10.clusters.42paris.fr>
2025-07-19 20:11:28 +0200

    ex06

--- Python_0/ex06/filterstring.py
+++ Python_0/ex06/filterstring.py
@@ -1,15 +1,30 @@
 import sys
+from ft_filter import ft_filter
+
+
+def longerThen(n):
+    """
+    returns a lamda function that checks if a word is onger than n characters
+    """
+    return lambda word: len(word) > n
 
 
 def main():
     """
-    __doc_
-    write something here"""
+    write something here
+    """
     args = sys.argv
+
     try:
+        assert len(args) == 3
         S = args[1]
         N = int(args[2])
-    except AssertionError("he arguments are bad"):
-        print("problem")
+    except Exception:
+        raise AssertionError("the arguments are bad")
+    words = S.split()
+    res = [word for word in ft_filter(longerThen(N), words)]
+    print(res)
+
+
 if __name__ == "__main__":
     main()
--- Python_0/ex06/ft_filter.py
+++ Python_0/ex06/ft_filter.py
@@ -1,1 +1,10 @@
-
+def ft_filter(function, iterable):
+    """
+    function: a function to apply to each element of the iterable(or None)
+    iterable: a sequence, iterator, or a container to filter
+    return: an iterator with elements for which function(element) is True,
+            or elements that are evaluated True if finction is None
+    """
+    if function:
+        return (item for item in iterable if function(item))
+    return (item for item in iterable if item)
