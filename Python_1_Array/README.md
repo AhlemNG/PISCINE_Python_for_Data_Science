91b40564113c09933e7f04554fa7c8beec8700b3
anouri <ahlem.nouri.gallali@gmail.com>
2025-10-26 12:43:30 +0100

    env

---
+++ Python_1_Array/README.md
@@ -0,0 +1,22 @@
+# create a virtual environment
+
+python -m venv /path/to/new/virtual/myenv
+
+# activate the virtual environment
+
+source /path/to/new/virtual/myenv/bin/activate
+
+# install requirements
+
+pip install -r requirements.txt
+
+# create/update requirements.txt
+
+pip freeze > requirements.txt
+
+# deactivate the virtual environment
+
+deactivate
+
+
+
--- Python_1_Array/ex02/load_image.py
+++ Python_1_Array/ex02/load_image.py
@@ -1,19 +1,16 @@
 import numpy as np
 import PIL.Image as Image
-import os as os
 
 
-os.environ["PYTHONWARNINGS"] = "ignore"
 def ft_load(path: str) -> list:
     """
     loads an image based on a given path
     prints its format and its content RGB
+    returns an array of pixels
     """
-
     img = Image.open(path)
     img.show()
-    img_arr = np.array(img)    
-    print("The shape of image is: ",img_arr.shape)
+    img_arr = np.array(img)
+    print("The shape of image is: ", img_arr.shape)
     pixels = np.array(img.getdata())
-    print(pixels)
-    return(pixels)
+    return (pixels)
