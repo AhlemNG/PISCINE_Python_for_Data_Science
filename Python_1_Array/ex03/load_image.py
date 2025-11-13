da750cd5e1b9ef2e44ea075e964f3606eb20c221
Ahlem NOURI <anouri@f4r7s7.clusters.42paris.fr>
2025-11-13 17:35:04 +0100

    grayscale

--- Python_1_Array/ex03/load_image.py
+++ Python_1_Array/ex03/load_image.py
@@ -1,22 +1,17 @@
 import numpy as np
-from PIL import Image
+import matplotlib.pyplot as mpl
 
-def ft_load(path: str) -> np.ndarray:
+
+def ft_load(path: str) -> np.array:
     """
     loads an image based on a given path
     prints its format and its content RGB
     returns an array of pixels
     """
     try:
-        img = Image.open(path)
-        img_arr = np.array(img) #ici image to array
-
-        print(f"The shape of image is: {img_arr.shape}")
-        # print(img_arr)
-        pixels = np.array(img.getdata()) #image data to array
-        print(pixels)
-        # return (pixels)
-        return img_arr
+        img = mpl.imread(path)
+        print("The shape of image is: ", img.shape)
+        return (img)
     except FileNotFoundError:
         print(f"Error: File '{path}' not found.")
     except Exception as e:
--- Python_1_Array/ex03/zoom.py
+++ Python_1_Array/ex03/zoom.py
@@ -2,16 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from load_image import ft_load
 
-
-import numpy as np
-import matplotlib.pyplot as plt
-
 def to_gray(img: np.ndarray) ->np.array:
     """
     returns a grayscaled image based on a given image,
     """
-
-
     if img.ndim == 3:
         gray = img[:, :, 0] * 0.2989 + img[:, :, 1] * 0.5870 \
         + img[:, :, 2] * 0.1140
@@ -26,23 +20,36 @@ def zoom_image(array: np.ndarray, y_start, y_end, x_start, x_end):
     try:
         if array is None:
             raise ValueError("No image loaded.")
-        zoomed = array[y_start:y_end, x_start:x_end]
-        print(f"New shape after slicing: {zoomed.shape}")
-        print(zoomed)
-        plt.imshow(zoomed)
-        plt.show()
     except Exception as e:
         print(f"Error while zooming: {e}")
+
+def zoom_image(array: np.ndarray):
+    """
+    Zooms ap part of a given image using slicing methodand shows it with axes.
+    """
+    try:
+        if array is None:
+            raise ValueError("No image loaded.")
+        h = array.shape[0]
+        w = array.shape[1]
+        zoomed = array[h//4:h//4 + 400, w//4:w//4 + 400]
+        print(f"New shape after slicing: {zoomed.shape}")
+        return(zoomed)
+    except Exception as e:
+        print("an error occurred: ", e)
 
 
 def main():
     path = "animal.jpeg"
-    img = to_gray(ft_load(path))
-
+    img = ft_load(path)
     if img is not None:
-        h = img.shape[0]
-        w = img.shape[1]
-        zoom_image(img, h//4, h//4 + 400, w//4, w//4 + 400)
+        print(img)
+        gray = to_gray(img)
+        zoomed = zoom_image(gray)
+        print(zoomed)
 
+        plt.imshow(zoomed, cmap='gray', vmin=0, vmax=255)
+        plt.show()
+        
 if __name__ == "__main__":
     main()
